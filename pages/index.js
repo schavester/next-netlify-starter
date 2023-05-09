8f93a6aa2b305a5c310bc4185e0d334fb27758ea
Richard Schave <schavester@gmail.com>
2023-05-08 17:09:08 -0700

    added image and updated title

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Save LACMA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Save LACMA" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+    <img src="https://res.cloudinary.com/on-bunker-hill/image/upload/f_auto/q_auto/v1683590336/savelacma-logo-alt-web_l8yxb9.png">
         </p>
       </main>
 
